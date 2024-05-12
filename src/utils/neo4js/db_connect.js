0504485bdced7625281b2ee909a474b7c1bcb97c
unknown <iamblessedshammah@gmail.com>
2024-05-12 19:48:44 +0300

    Database Connection Success!!!

---
+++ src/utils/neo4js/db_connect.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+var neo4j = require('neo4j-driver');
+
+(async () => {
+  // URI examples: 'neo4j://localhost', 'neo4j+s://xxx.databases.neo4j.io'
+  const URI = "neo4j+s://38626270.databases.neo4j.io";
+  const USER = "neo4j";
+  const PASSWORD = "norTk7knkhQPNkYGcHG1IfKrO1_iciGb35RR_ImLP5k";
+  let driver
+
+  try {
+    driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD))
+    const serverInfo = await driver.getServerInfo()
+    console.log('Connection established')
+    console.log(serverInfo)
+  } catch(err) {
+    console.log(`Connection error\n${err}\nCause: ${err.cause}`)
+  }
+})();
