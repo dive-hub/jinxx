741b46c25b33f6db66549c8c4cb47a68c3548588
unknown <iamblessedshammah@gmail.com>
2024-06-12 14:45:29 +0300

    Login fix

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { BsGoogle, BsTwitterX } from "react-icons/bs";
 import { AuthContext } from './AuthContext';
-import '../App.css'
+import '../App.css';
 
 function LogIn() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const { login } = useContext(AuthContext);
+  const history = useHistory();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,16 +24,16 @@ function LogIn() {
     const data = await response.json();
     if (data.success) {
       login(data.token);
-      {/**Add redirect to User Dashboard add modol close here*/}
-      
+      history.push('/dashboard'); // Redirect to User Dashboard
+      // Add modal close logic if applicable
     } else {
       console.log('Login failed');
-      alert('wrong details')
+      alert('wrong details');
     }
   };
 
   return (
-    <div className='signup-container; containergoogle'>
+    <div className='signup-container containergoogle'>
       <form onSubmit={handleSubmit}>
         <div>
           <input
@@ -55,10 +57,10 @@ function LogIn() {
           />
         </div>
         <div className="d-grid gap-2">
-        <Button variant="primary" size="lg" className='button' type='submit'>
-          Log In
-        </Button>
-        <a className='logpa' href='./Home'>Forgot password?</a>
+          <Button variant="primary" size="lg" className='button' type='submit'>
+            Log In
+          </Button>
+          <a className='logpa' href='./Home'>Forgot password?</a>
         </div>
         
         <Container className='containergoogle'>
